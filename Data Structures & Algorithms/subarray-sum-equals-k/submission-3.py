9fce52c763c577da252aff4c0fc729d4bd886f84
Ganavi Purushothama <PES2UG24CS167@pesuonline.onmicrosoft.com>
2026-06-21 14:36:47 +0530

    Add: subarray-sum-equals-k - submission-3

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-3.py
@@ -0,0 +1,32 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        # Number of valid subarrays found so far
+        count = 0
+        # Running total (prefix sum)
+        prefix = 0
+        # Dictionary storing:
+        # prefix_sum : number of times we've seen it
+        #
+        # We start with {0:1}
+        # because before reading any numbers,
+        # our prefix sum is 0 once.
+        seen = {0: 1}
+        for num in nums:
+            prefix += num
+            # Ask:
+            # "Have I seen a prefix sum equal to
+            # (current prefix - k) before?"
+            # If yes, then a subarray summing to k exists.
+            if prefix - k in seen:
+                # Add however many times we've seen it
+                count += seen[prefix - k]
+            # Record current prefix sum in dictionary
+            
+            # If prefix already exists,
+            # increase its frequency by 1.
+            
+            # Otherwise start it at 1.
+            seen[prefix] = seen.get(prefix, 0) + 1
+
+        # Return total number of valid subarrays
+        return count
